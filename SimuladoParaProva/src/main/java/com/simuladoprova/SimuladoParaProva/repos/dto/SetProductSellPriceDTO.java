3e0c1e61d7d5b8a44eb4b53b02686de686e0187a
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 12:07:09 -0300

    DTO for setting the price which the product was/will be sold at.

--- SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/dto/SetProductSellPriceDTO.java
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/dto/SetProductSellPriceDTO.java
@@ -1,5 +1,24 @@
 package com.simuladoprova.SimuladoParaProva.repos.dto;
 
 public class SetProductSellPriceDTO {
+	private int product_id;
+	private float salePrice;
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public float getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(float salePrice) {
+		this.salePrice = salePrice;
+	}
+	
 
 }
