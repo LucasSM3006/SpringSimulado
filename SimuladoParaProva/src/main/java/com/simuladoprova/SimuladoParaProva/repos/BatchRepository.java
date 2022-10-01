cd648270335dcfb2123cb9fc8fea93cd5db288b3
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 16:17:26 -0300

    Added custom query for getting all results by name.

--- SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/BatchRepository.java
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/BatchRepository.java
@@ -1,9 +1,15 @@
 package com.simuladoprova.SimuladoParaProva.repos;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.simuladoprova.SimuladoParaProva.model.Batch;
 
 public interface BatchRepository extends CrudRepository<Batch, Integer> {
-
+	
+	@Query("from Batch where name = :name")
+	Optional<Batch> findByName(@Param("name") String name);
 }
