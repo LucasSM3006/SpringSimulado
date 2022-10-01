084ad05099ef8bf9d49d4a55db6e0ae6d03323a6
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 14:46:07 -0300

    Added custom SQL queries to be able to get precise information back. By availability and by category.

--- SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/ProductRepository.java
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.simuladoprova.SimuladoParaProva.repos;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.simuladoprova.SimuladoParaProva.model.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+	@Query(value = "SELECT * FROM product WHERE available = true", nativeQuery = true)
+	List<Product> findAllAvailableProducts();
+	
+	@Query("from Product where category = :category")
+	List<Product> findAllByCategory(@Param("category") String category);
 }
