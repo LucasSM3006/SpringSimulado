05255eb78f0d785b853c9c24d79b2c7afc1800d1
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 16:17:04 -0300

    Forgot to add the getters and setters.

--- SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/model/Batch.java
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/model/Batch.java
@@ -2,7 +2,10 @@ package com.simuladoprova.SimuladoParaProva.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -12,11 +15,47 @@ import javax.persistence.Table;
 @Table
 public class Batch {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int batch_id;
 	private String name;
+	@Column(name = "batch_date")
 	private Date date;
 
 	@ManyToOne
-	@JoinColumn(name = "product_id")
+	@JoinColumn(name = "product")
 	private Product product;
+
+	public int getBatch_id() {
+		return batch_id;
+	}
+
+	public void setBatch_id(int batch_id) {
+		this.batch_id = batch_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
 }
