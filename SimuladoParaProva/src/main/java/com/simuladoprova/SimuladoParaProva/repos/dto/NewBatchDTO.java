c44d9a818fda2f9e5b4652656a75d1723ed69ec7
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 16:15:35 -0300

    DTOs for the batches.

---
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/dto/NewBatchDTO.java
@@ -0,0 +1,43 @@
+package com.simuladoprova.SimuladoParaProva.repos.dto;
+
+import java.util.Date;
+
+public class NewBatchDTO {
+	private int batch_id;
+	private String name;
+	private Date date;
+	private int product_id;
+
+	public int getBatch_id() {
+		return batch_id;
+	}
+
+	public void setBatch_id(int batch_id) {
+		this.batch_id = batch_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+}
