b4f93b579e5f0797446f2d7318c3dc568a684864
LucasSM3006 <lucas.sasso@valtech.com>
2022-10-01 17:11:04 -0300

    Added the query to search by date.

--- SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/BatchRepository.java
+++ SimuladoParaProva/src/main/java/com/simuladoprova/SimuladoParaProva/repos/BatchRepository.java
@@ -1,5 +1,7 @@
 package com.simuladoprova.SimuladoParaProva.repos;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,7 @@ public interface BatchRepository extends CrudRepository<Batch, Integer> {
 	
 	@Query("from Batch where name = :name")
 	Optional<Batch> findByName(@Param("name") String name);
+	
+	@Query("from Batch where date = :date")
+	List<Batch> findByDate(@Param("date") LocalDate date);
 }
